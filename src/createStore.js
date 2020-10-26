bb9a0b2d1d66d87c452671af057ca4dda0a09594
kryptondv <kryptondevpl@gmail.com>
2020-10-26 21:41:40 +0100

    format code

--- src/createStore.js
+++ src/createStore.js
@@ -2,9 +2,4 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from './reducers';
 
-
-
-export const store = createStore(
-  RootReducer,
-  applyMiddleware(thunk),
-);
+export const store = createStore(RootReducer, applyMiddleware(thunk));
