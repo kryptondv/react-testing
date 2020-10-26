c03889adc156335e214a6af58ac6e33b10de002d
kryptondv <kryptondevpl@gmail.com>
2020-10-26 16:39:21 +0100

    add Headline class component with tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from './components/header/Header';
+import Headline from './components/headline/Headline';
 import './app.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className="main">
+        <Headline header="Posts" desc="Click button to render posts"/>
+      </main>
     </div>
   );
 }
---
+++ src/components/headline/Headline.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Headline from './Headline';
+
+import { findByTestAtr } from '../../../utils';
+
+const setUp = (props = {}) => {
+  const component = shallow(<Headline {...props} />);
+  return component;
+};
+
+describe('Headline Component', () => {
+  describe('Have props', () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = {
+        header: 'Test Header',
+        desc: 'Test Desc',
+      };
+      wrapper = setUp(props);
+    });
+
+    test('Should render without errors', () => {
+      const component = findByTestAtr(wrapper, 'HeadlineComponent')
+      expect(component.length).toBe(1);
+    })
+
+    test('Should render a h1', () => {
+      const h1 = findByTestAtr(wrapper, 'title')
+      expect(h1.length).toBe(1);
+    })
+
+    test('Should render description', () => {
+      const desc = findByTestAtr(wrapper, 'desc')
+      expect(desc.length).toBe(1);
+    })
+  });
+
+  describe('Have no props', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setUp();
+    });
+
+    test('Should not render', () => {
+      const component = findByTestAtr(wrapper, 'HeadlineComponent');
+      expect(component.length).toBe(0);
+    });
+  });
+});
