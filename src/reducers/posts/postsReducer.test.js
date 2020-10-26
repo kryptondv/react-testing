897d474329aef46611596d42e501cffdc9e41425
kryptondv <kryptondevpl@gmail.com>
2020-10-26 22:02:25 +0100

    add postsReducer with tests

---
+++ src/reducers/posts/postsReducer.test.js
@@ -0,0 +1,19 @@
+import { types } from '../../actions/types.js';
+import postsReducer from './postsReducer';
+
+describe('Posts Reducer', () => {
+  test('Should return default state', () => {
+    const newState = postsReducer(undefined, {});
+    expect(newState).toEqual([]);
+  });
+
+  test('Should return new state if receiving action', () => {
+    const posts = [
+      { title: 'Test 1' },
+      { title: 'Test 2' },
+      { title: 'Test 3' },
+    ];
+    const newState = postsReducer(undefined, { type: types.GET_POSTS, payload: posts});
+    expect(newState).toEqual(posts);
+  });
+});
